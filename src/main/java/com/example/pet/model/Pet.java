f5639d90420b59eb3d72eef500e5f91291b62866
otinu <fitshter@gmail.com>
2022-10-01 08:46:18 +0900

    [fix]DB

--- src/main/java/com/example/pet/model/Pet.java
+++ src/main/java/com/example/pet/model/Pet.java
@@ -2,13 +2,20 @@ package com.example.pet.model;
 
 import java.sql.Date;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,32 +23,45 @@ import lombok.NoArgsConstructor;
 public class Pet {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // これを付けるカラムSerialにする必要がある
+	@Column(name = "id")
 	private Integer id;
 	
 	/*
 	 *  User has many Pet にするための外部キー
 	 *  外部キー制約をもたせたい
 	 *  ⇒UserId 1番を「no_owner」などに設定し、目撃情報を投稿する際はno_ownerと結びつける
-	 *  
+	 *  　⇒⇒no_ownerのデータ群は「eyewitness report」テーブルと見なして扱いたい
 	 */
 	//private Integer user_id;
 	
-	/* Data JPA に切り替えるか検討
 	@ManyToOne
 	@JoinColumn(name = "user_id")
-	*/
 	private User user;
 	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "age")
 	private Integer age;
+	
+	@Column(name = "sex")
 	private Boolean sex;
-	//private String charm_point; // 目視でないと確認しにくい情報などを共有できるようにする
+	
+	private String charm_point; // 目視でないと確認しにくい情報などを共有できるようにする
+	
+	@Column(name = "post_cord") // おばあちゃんの犬を別居の娘が代わりに探している場合など、Userに郵便番号もたせると不便になりそう
 	private String post_cord;
+	
+	@Column(name = "address")
 	private String address;
-	private String comment;		// コメントはPet has many Commentのエンティティに修正したい
-	private String contact;		// コンタクト・サブコンタクトはUserテーブルへ移行
-	private String sub_contact; // 			〃
+	
+	@Column(name = "image")
 	private String image;
+	
+	@Column(name = "created_at")
 	private Date created_at;
+	
+	@Column(name = "updated_at")
 	private Date updated_at;
 }
--- src/main/java/com/example/pet/model/User.java
+++ src/main/java/com/example/pet/model/User.java
@@ -1,25 +1,52 @@
 package com.example.pet.model;
 
 import java.sql.Date;
+import java.util.List;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "user")
+@ToString(exclude = "petList")
 public class User {
 
 	@Id
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "message")
 	private String message;		// 飼い主からの発信に一定の自由度をもたせたい
+	
+	@Column(name = "contact")
 	private String contact;		// Petから持ち越し
+	
+	@Column(name = "sub_contact")
 	private String sub_contact; // 	   〃
 	
+	@Column(name = "created_at")
 	private Date created_at;
+	
+	@Column(name = "updated_at")
 	private Date updated_at;
+	
+	// mappedByには、1対N の1側を指定
+	// cascadeには、1対N の1側の変更(更新や削除など)に伴って、N側も同様の処理を連動させるかを指定
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Pet> petList;
 }
