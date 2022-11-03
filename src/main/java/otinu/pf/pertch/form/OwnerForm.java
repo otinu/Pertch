3eba69ccdce9c87d8a11c74b670a1ed1fbd0d1ae
otinu <fitshter@gmail.com>
2022-11-03 15:31:46 +0900

    [add]Comment

--- src/main/java/otinu/pf/pertch/form/OwnerForm.java
+++ src/main/java/otinu/pf/pertch/form/OwnerForm.java
@@ -1,7 +1,6 @@
 package otinu.pf.pertch.form;
 
 import java.sql.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
@@ -12,34 +11,40 @@ import lombok.Data;
 @Data
 public class OwnerForm {
 	
+	/* ID */
 	@Id
 	@Column(name = "id")
 	@NotBlank
 	private Integer id;
 	
+	/* ログイン用メールアドレス */
 	@Column(name = "username")
 	private String username;
 	
+	/* ログイン用パスワード */
 	@Column(name = "password")
 	private String password;
 	
+	/* 名前 */
 	@Column(name = "name")
 	private String name;
 	
+	/* メッセージ */
 	@Column(name = "message")
 	private String message;
 	
+	/* 連絡先 */
 	@Column(name = "contact")
 	private String contact;
 
+	/* 登録日時 */
 	@Column(name = "created_at")
 	private Date created_at;
 	
+	/* 更新日時 */
 	@Column(name = "updated_at")
 	private Date updated_at;
 	
-	private List<PetForm> petList;
-
-	private PetForm newPet;
+	// private List<PetForm> petList;
 
 }
--- src/main/java/otinu/pf/pertch/form/PetForm.java
+++ src/main/java/otinu/pf/pertch/form/PetForm.java
@@ -2,6 +2,10 @@ package otinu.pf.pertch.form;
 
 import java.sql.Timestamp;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,23 +15,38 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PetForm {
 	
+	/* ID */
 	private Integer id;
 	
+	/* 名前 */
+	@NotBlank(message="名前の入力が必要です")
+	@Size(min = 2, max = 20, message = "{min}-{max}文字まで")
 	private String name;
 	
+	/* 年齢 */
+	//@NotNull	// Integerの場合はNotNull
 	private Integer age;
 	
+	/* 性別 */
+	@NotNull
 	private Boolean sex;
 	
+	/* 特徴 */
 	private String charmPoint;
 	
+	/* 住処の郵便番号 */
+	@NotBlank
 	private String postCord;
 	
+	/* 住処の住所 */
 	private String address;
 	
+	/* 飼い主 */
 	private String image;
 	
+	/* 登録日時 */
 	private Timestamp createdAt;
 	
+	/* 更新日時 */
 	private Timestamp updatedAt;
 }
