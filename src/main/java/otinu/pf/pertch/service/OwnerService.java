9cfcf7ff93e849ba2d1e79b48ef2a6a43e2fa2e3
otinu <fitshter@gmail.com>
2023-06-13 20:28:35 +0900

    owner/mypageへの画面遷移機能を追加しました

--- src/main/java/otinu/pf/pertch/service/OwnerService.java
+++ src/main/java/otinu/pf/pertch/service/OwnerService.java
@@ -3,17 +3,26 @@ package otinu.pf.pertch.service;
 import java.security.Principal;
 
 import otinu.pf.pertch.entity.Owner;
+import otinu.pf.pertch.form.OwnerForm;
 
 public interface OwnerService {
-	
+
 	public void insertOwner(String username, String password, String name, String message, String contact);
-	
+
 	// public Owner updateOwner(Integer id);
-	
+
 	public Owner findByOwnerId(Integer id);
-	
+
 	public Owner findByName(String name);
-	
+
 	public Owner getCurrentUser(Principal principal);
 	
+	public void updateOwner(Owner owner);
+
+	OwnerForm makeOwnerForm(Owner owner);
+
+	OwnerForm setOwnerToForm(OwnerForm ownerForm, Owner owner);
+
+	Owner makeOwner(OwnerForm ownerForm, Owner currentUser);
+
 }
